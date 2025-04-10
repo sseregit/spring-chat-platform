09f562ebb6eef16683eed810585e0cebb2e8a528
장연식 <sseregit@knowwheresoft.com>
2025-04-10 15:21:51 +0900

    initializer & dependencies build

---
+++ src/main/java/io/github/sseregit/springchatplatform/SpringChatPlatformApplication.java
@@ -0,0 +1,13 @@
+package io.github.sseregit.springchatplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringChatPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/sseregit/springchatplatform/SpringChatPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sseregit.springchatplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
