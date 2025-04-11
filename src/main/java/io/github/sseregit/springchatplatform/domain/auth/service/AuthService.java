b2075432134982a492009f8465aa865420226643
장연식 <sseregit@knowwheresoft.com>
2025-04-11 14:26:22 +0900

    JPA를 활용한 Service 구성하기

--- src/main/java/io/github/sseregit/springchatplatform/domain/auth/service/AuthService.java
+++ src/main/java/io/github/sseregit/springchatplatform/domain/auth/service/AuthService.java
@@ -1,9 +1,15 @@
 package io.github.sseregit.springchatplatform.domain.auth.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import io.github.sseregit.springchatplatform.domain.auth.model.request.CreateUserRequest;
 import io.github.sseregit.springchatplatform.domain.auth.model.response.CreateUserResponse;
+import io.github.sseregit.springchatplatform.domain.repository.UserRepository;
+import io.github.sseregit.springchatplatform.domain.repository.entity.User;
+import io.github.sseregit.springchatplatform.domain.repository.entity.UserCredentials;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,7 +18,40 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class AuthService {
 
+    private final UserRepository userRepository;
+
+    @Transactional(transactionManager = "createUserTransactionManager")
     public CreateUserResponse createUser(CreateUserRequest request) {
+        Optional<User> user = userRepository.findByName(request.name());
+
+        if (user.isPresent()) {
+
+        }
+
+        try {
+            User newUser = newUser(request.name());
+            UserCredentials userCredentials = newUserCredentials(request.password(), newUser);
+            newUser.setUserCredentials(userCredentials);
+
+            User savedUser = userRepository.save(newUser);
+
+            if (savedUser == null) {
+
+            }
+        } catch (Exception e) {
+
+        }
+
         return new CreateUserResponse(request.name());
+    }
+
+    private User newUser(String name) {
+        return User.builder()
+            .name(name)
+            .build();
+    }
+
+    private UserCredentials newUserCredentials(String password, User user) {
+        return null;
     }
 }
--- src/main/java/io/github/sseregit/springchatplatform/domain/repository/UserRepository.java
+++ src/main/java/io/github/sseregit/springchatplatform/domain/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package io.github.sseregit.springchatplatform.domain.repository;
 
-public interface UserRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.sseregit.springchatplatform.domain.repository.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByName(String name);
+
+    boolean existsByName(String name);
 }
