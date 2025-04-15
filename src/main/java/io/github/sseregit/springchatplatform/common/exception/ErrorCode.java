a2fccf93c20df51260d33f287f97e9e7c44f682e
장연식 <sseregit@knowwheresoft.com>
2025-04-15 11:35:25 +0900

    에러 핸들링을 활용한 JWT Verify

--- src/main/java/io/github/sseregit/springchatplatform/common/exception/ErrorCode.java
+++ src/main/java/io/github/sseregit/springchatplatform/common/exception/ErrorCode.java
@@ -9,6 +9,10 @@ public enum ErrorCode implements CodeInterface {
     USER_SAVED_FAILED(-2, "User saved failed"),
     NOT_EXIST_USER(-3, "User not exists"),
     MISS_MATCH_PASSWORD(-4, "Miss match password"),
+
+    TOKEN_IS_INVALID(-200, " token is invalid"),
+    ACCESS_TOKEN_IS_NOT_EXPIRED(-201, "Access token is not expired"),
+    TOKEN_IS_EXPIRED(-202, "token is expired"),
     ;
 
     private final Integer code;
--- src/main/java/io/github/sseregit/springchatplatform/security/JWTProvider.java
+++ src/main/java/io/github/sseregit/springchatplatform/security/JWTProvider.java
@@ -1,5 +1,7 @@
 package io.github.sseregit.springchatplatform.security;
 
+import static io.github.sseregit.springchatplatform.common.exception.ErrorCode.*;
+
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
@@ -7,7 +9,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.AlgorithmMismatchException;
+import com.auth0.jwt.exceptions.InvalidClaimException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
+import io.github.sseregit.springchatplatform.common.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -37,4 +45,37 @@ public class JWTProvider {
             .sign(Algorithm.HMAC256(refreshKey));
     }
 
+    public DecodedJWT checkAccessForRefresh(String token) {
+        try {
+            DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC256(secretKey)).build().verify(token);
+            log.error("token must be expired: {}", decodedJWT.getSubject());
+            throw new CustomException(ACCESS_TOKEN_IS_NOT_EXPIRED);
+        } catch (AlgorithmMismatchException | SignatureVerificationException | InvalidClaimException e) {
+            throw new CustomException(TOKEN_IS_INVALID);
+        } catch (TokenExpiredException e) {
+            return JWT.decode(token);
+        }
+    }
+
+    public DecodedJWT decodedAccessToken(String token) {
+        return decodedTokenAfterVerify(token, secretKey);
+    }
+
+    public DecodedJWT decodedRefreshToken(String token) {
+        return decodedTokenAfterVerify(token, refreshKey);
+    }
+
+    private DecodedJWT decodedTokenAfterVerify(String token, String key) {
+        try {
+            return JWT.require(Algorithm.HMAC256(key)).build().verify(token);
+        } catch (AlgorithmMismatchException | SignatureVerificationException | InvalidClaimException e) {
+            throw new CustomException(TOKEN_IS_INVALID);
+        } catch (TokenExpiredException e) {
+            throw new CustomException(TOKEN_IS_EXPIRED);
+        }
+    }
+
+    public DecodedJWT decodedJWT(String token) {
+        return JWT.decode(token);
+    }
 }
