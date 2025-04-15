79d886346799f83041fc2ac93f1f04b1b0327bd9
장연식 <sseregit@knowwheresoft.com>
2025-04-15 14:27:36 +0900

    Protocol을 적용하는 WebSocket 및 Long Poiing

--- src/main/java/io/github/sseregit/springchatplatform/config/WssConfig.java
+++ src/main/java/io/github/sseregit/springchatplatform/config/WssConfig.java
@@ -1,21 +1,30 @@
 package io.github.sseregit.springchatplatform.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration(proxyBeanMethods = false)
 @EnableWebSocket
 @RequiredArgsConstructor
-public class WssConfig implements WebSocketConfigurer {
+public class WssConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(null, "/ws/v1/chat")
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp")
             .setAllowedOrigins("*");
+        // 클라이언트가 웹소켓을 사용할 수 없는 환경에 대해서 서버 간 웹소켓 통신에 대해 대체제로서 구현을 해주는 방어 로직
+        // .withSockJS();
     }
 
 }
